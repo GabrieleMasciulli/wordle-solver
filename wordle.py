f78fee4d78e22d0251eb74656c0585124ab0dc3f
Gabriele Masciulli <gmasciu@gmail.com>
2022-02-17 12:40:26 +0100

    Computing patterns with less verbose code, created function to check if a possible word matches the pattern for a given allowed word and counting the number of possible words left from a given allowed word and a pattern

--- wordle.py
+++ wordle.py
@@ -1,49 +1,86 @@
-import json
-from math import log2
+POSSIBLE_WORDS_FILE = './data/possible_words.txt'
+ALLOWED_WORDS_FILE = './data/allowed_words.txt'
 
 
-def countWordsLeftFromPattern(word, pattern):
+def get_word_list(all):  # returns a list of strings
+    result = []
+
+    # loading either the allowed or possible words
+    file = ALLOWED_WORDS_FILE if all else POSSIBLE_WORDS_FILE
+    with open(file):
+        result.extend([word.strip() for word in open(file).readlines()])
+
+    return result
+
+
+def word_has_letter_n_times(word, letter, occurences):
+    return word.count(letter) == occurences
+
+# returns true if the word passed as a parameter matches the given patter, false otherwise
+
+
+def matches_pattern(word, possible_word, pattern):
+    match = False
+
+    for i, color in enumerate(pattern):
+        if color == 'black':
+            match = possible_word.find(word[i]) == -1
+        elif color == 'green':
+            # letter at index i MUST correspond
+            match = possible_word[i] == word[i]
+        elif color == 'yellow':
+            ''
+
+        if not match:
+            break
+
+    return match
+
+
+def count_words_left_from_pattern(word, pattern, possible_words):
     count = 0
 
-    return
+    # print(pattern)
 
-# in wordle a pattern is an assignment for each letter in the word of either the color green, yellow or grey,
-# respectively meaning that the letter was in the right position, it occured at least once in the word but in the wrong position
-# and it wasn't at all in the word.
+    for possible_word in possible_words:
+        if matches_pattern(word, possible_word, pattern):
+            count += 1
+            # print(f'Word: {word}, Possible word: {possible_word}')
+
+    return count
+
+# in wordle a pattern is an assignment for each letter in the word of either the color green,
+# yellow or grey, respectively meaning that the letter was in the right position, it occured
+# at least once in the word but in the wrong position and it wasn't at all in the word.
 
 
-def computePatterns():
+def compute_patterns():  # returns a list of tuples (each tuple represents a specific pattern)
     colors = ['black', 'yellow', 'green']
 
     patterns = []
 
-    for l_1 in range(3):
-        for l_2 in range(3):
-            for l_3 in range(3):
-                for l_4 in range(3):
-                    for l_5 in range(3):
-                        pattern = (colors[l_1], colors[l_2],
-                                   colors[l_3], colors[l_4], colors[l_5])
-                        patterns.append(pattern)
-
     # less verbose way to create the pattern array
-    # patterns = [(a, b, c, d, e)
-    #             for a in colors for b in colors for c in colors for d in colors for e in colors]  # all possible patterns for a 5-letter word
+    patterns = [(a, b, c, d, e)
+                for a in colors for b in colors for c in colors for d in colors for e in colors]  # all possible patterns for a 5-letter word
     return patterns
 
 
 def main():
-    word = "trews"
-    patterns = computePatterns()
+    word = "never"
+    patterns = compute_patterns()
+    possible_words = get_word_list(all=False)
 
     # array mapping the number of words left for each pattern related to a given word
-    words_left = []
+    # words_left = []
 
-    for p in patterns:
-        words_left.append(
-            {'pattern': p, 'value': countWordsLeftFromPattern(word, p)})
+    # for pattern in patterns:
+    #     words_left.append(
+    #         {'pattern': pattern, 'value': count_words_left_from_pattern(word, pattern, possible_words)})
 
-    print(words_left)
+    for pattern in patterns:
+        words_left_count = count_words_left_from_pattern(
+            word, pattern, possible_words)
+        print(f'Pattern: {pattern}, Words left: {words_left_count}')
 
 
 main()
